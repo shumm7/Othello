e19c4f716783042c179e394aa57c3deb10f98b20
しゅう <44467870+shumm7@users.noreply.github.com>
2019-08-23 23:30:20 +0900

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Othello
+{
+    partial class Othello
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Othello));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.TipLabel = new System.Windows.Forms.Label();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.LabelAmount = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CurrentColorDisplayPanel = new System.Windows.Forms.Panel();
+            this.CanPlaceLabel = new System.Windows.Forms.Label();
+            this.MousePos = new System.Windows.Forms.Label();
+            this.Position = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(640, 640);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawBoard);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseClicked);
+            this.panel1.MouseLeave += new System.EventHandler(this.MouseLeft);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MouseMoved);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.TipLabel);
+            this.panel2.Controls.Add(this.TurnLabel);
+            this.panel2.Controls.Add(this.LabelAmount);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.CurrentColorDisplayPanel);
+            this.panel2.Controls.Add(this.CanPlaceLabel);
+            this.panel2.Controls.Add(this.MousePos);
+            this.panel2.Controls.Add(this.Position);
+            this.panel2.Controls.Add(this.reset);
+            this.panel2.Location = new System.Drawing.Point(12, 658);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(640, 85);
+            this.panel2.TabIndex = 1;
+            // 
+            // TipLabel
+            // 
+            this.TipLabel.AutoSize = true;
+            this.TipLabel.Location = new System.Drawing.Point(275, 45);
+            this.TipLabel.Name = "TipLabel";
+            this.TipLabel.Size = new System.Drawing.Size(0, 15);
+            this.TipLabel.TabIndex = 11;
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.AutoSize = true;
+            this.TurnLabel.Font = new System.Drawing.Font("Meiryo UI", 11F);
+            this.TurnLabel.Location = new System.Drawing.Point(9, 45);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(18, 19);
+            this.TurnLabel.TabIndex = 10;
+            this.TurnLabel.Text = "0";
+            // 
+            // LabelAmount
+            // 
+            this.LabelAmount.AutoSize = true;
+            this.LabelAmount.Font = new System.Drawing.Font("Meiryo UI", 20F);
+            this.LabelAmount.Location = new System.Drawing.Point(107, 45);
+            this.LabelAmount.Name = "LabelAmount";
+            this.LabelAmount.Size = new System.Drawing.Size(136, 35);
+            this.LabelAmount.TabIndex = 9;
+            this.LabelAmount.Text = "黒:2 白:2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "手番";
+            // 
+            // CurrentColorDisplayPanel
+            // 
+            this.CurrentColorDisplayPanel.Location = new System.Drawing.Point(47, 30);
+            this.CurrentColorDisplayPanel.Name = "CurrentColorDisplayPanel";
+            this.CurrentColorDisplayPanel.Size = new System.Drawing.Size(50, 50);
+            this.CurrentColorDisplayPanel.TabIndex = 6;
+            this.CurrentColorDisplayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.DrawCurrentColor);
+            // 
+            // CanPlaceLabel
+            // 
+            this.CanPlaceLabel.AutoSize = true;
+            this.CanPlaceLabel.Font = new System.Drawing.Font("Meiryo UI", 14.25F);
+            this.CanPlaceLabel.Location = new System.Drawing.Point(232, 3);
+            this.CanPlaceLabel.Name = "CanPlaceLabel";
+            this.CanPlaceLabel.Size = new System.Drawing.Size(99, 24);
+            this.CanPlaceLabel.TabIndex = 5;
+            this.CanPlaceLabel.Text = "範囲外です";
+            // 
+            // MousePos
+            // 
+            this.MousePos.AutoSize = true;
+            this.MousePos.Font = new System.Drawing.Font("Meiryo UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MousePos.Location = new System.Drawing.Point(109, 3);
+            this.MousePos.Name = "MousePos";
+            this.MousePos.Size = new System.Drawing.Size(73, 24);
+            this.MousePos.TabIndex = 4;
+            this.MousePos.Text = "(-1,-1)";
+            // 
+            // Position
+            // 
+            this.Position.AutoSize = true;
+            this.Position.Font = new System.Drawing.Font("Meiryo UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Position.Location = new System.Drawing.Point(9, 3);
+            this.Position.Name = "Position";
+            this.Position.Size = new System.Drawing.Size(94, 24);
+            this.Position.TabIndex = 3;
+            this.Position.Text = "X:-1 Y:-1";
+            // 
+            // reset
+            // 
+            this.reset.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.reset.Location = new System.Drawing.Point(476, 3);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(161, 38);
+            this.reset.TabIndex = 0;
+            this.reset.Text = "Reset";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 661);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // Othello
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 755);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(680, 794);
+            this.MinimumSize = new System.Drawing.Size(680, 794);
+            this.Name = "Othello";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Othello";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Position;
+        private System.Windows.Forms.Label MousePos;
+        private System.Windows.Forms.Label CanPlaceLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel CurrentColorDisplayPanel;
+        private System.Windows.Forms.Label LabelAmount;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Label TipLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Othello
+{
+    public partial class Othello : Form
+    {
+        public const int GameDisplay = 640;
+        public const int GridWidth = GameDisplay / 8;
+
+        public const int Black = BoardMain.Black;
+        public const int White = BoardMain.White;
+        public const int None = BoardMain.None;
+
+        BoardMain GameBoard = new BoardMain();
+
+
+
+        public Othello()
+        {
+            InitializeComponent();
+
+            GameBoard.ResetBoard();
+
+        }
+
+        private void DrawBoard(object sender, PaintEventArgs e)
+        {
+            SolidBrush greenBrush = new SolidBrush(Color.DarkGreen);
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+            //Board
+            e.Graphics.FillRectangle(greenBrush, 0, 0, GameDisplay, GameDisplay);
+            //Grid Line
+            for (int i = 0; i < 9; i++)
+            {
+                e.Graphics.FillRectangle(blackBrush, i*GridWidth - 1, 0, 2, GameDisplay);
+                e.Graphics.FillRectangle(blackBrush, 0, i * GridWidth - 1, GameDisplay, 2);
+            }
+            //Stone
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if(GameBoard.Color(x, y) == BoardMain.Black)
+                    {
+                        e.Graphics.FillEllipse(blackBrush, x * GridWidth + 10, y * GridWidth +10, GridWidth - 20, GridWidth - 20);
+                    }else if (GameBoard.Color(x, y) == BoardMain.White)
+                    {
+                        e.Graphics.FillEllipse(whiteBrush, x * GridWidth + 10, y * GridWidth + 10, GridWidth - 20, GridWidth - 20);
+                    }
+                }
+            }
+
+
+        }
+
+        private void MouseClicked(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = panel1.PointToClient(sp);
+
+            int posX, posY;
+            GameBoard.GetGridPosition(cp.X, cp.Y, GameDisplay, out posX, out posY);
+
+            if (GameBoard.CanPlace(GameBoard.CurrentColor, posX, posY) == true)
+            {
+                GameBoard.Place(GameBoard.CurrentColor, posX, posY);
+                GameBoard.Turn++;
+                GameBoard.ChangeTurn();
+
+                panel1.Refresh();
+                CurrentColorDisplayPanel.Refresh();
+
+                LabelAmount.Text = string.Format("黒:{0} 白:{1}", GameBoard.GetAmount(Black), GameBoard.GetAmount(White));
+                TurnLabel.Text = string.Format("{0}", GameBoard.Turn);
+                TipLabel.Text = "";
+
+                if (GameBoard.CheckGameEnd() == true)
+                {
+                    if (GameBoard.GetAmount(Black) > GameBoard.GetAmount(White))
+                    {
+                        TipLabel.Text = "黒の勝利でゲームは終了しました";
+                    } else if (GameBoard.GetAmount(Black) < GameBoard.GetAmount(White))
+                    {
+                        TipLabel.Text = "白の勝利でゲームは終了しました";
+                     }
+                    else if (GameBoard.GetAmount(Black) == GameBoard.GetAmount(White))
+                    {
+                        TipLabel.Text = "引き分けでゲームは終了しました";
+                    }
+                }
+                else
+                {
+                    if (GameBoard.CanPlaceMyColor(GameBoard.CurrentColor) == false) {
+                        GameBoard.Turn++;
+                        GameBoard.ChangeTurn();
+                        TipLabel.Text = string.Format("{0}番目の手はスキップされました", GameBoard.Turn - 1);
+                    }
+                    else
+                    {
+                        TipLabel.Text = "";
+                    }
+                }
+            }
+        }
+
+        private void MouseMoved(object sender, MouseEventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = panel1.PointToClient(sp);
+
+            int posMouseX = cp.X;
+            int posMouseY = cp.Y;
+            int posX, posY;
+            GameBoard.GetGridPosition(posMouseX, posMouseY, GameDisplay, out posX, out posY);
+
+            Position.Text = string.Format("X:{0} Y:{1}", posX, posY);
+            MousePos.Text = string.Format("({0},{1})", posMouseX, posMouseY);
+
+            if (GameBoard.CanPlace(GameBoard.CurrentColor, posX, posY) == true)
+            {
+                CanPlaceLabel.Text = "置けます";
+            } else
+            {
+                CanPlaceLabel.Text = "置けません";
+            }
+        }
+
+        private void MouseLeft(object sender, EventArgs e)
+        {
+            System.Drawing.Point sp = System.Windows.Forms.Cursor.Position;
+            System.Drawing.Point cp = panel1.PointToClient(sp);
+
+            int posMouseX = cp.X;
+            int posMouseY = cp.Y;
+            int posX, posY;
+            GameBoard.GetGridPosition(posMouseX, posMouseY, GameDisplay, out posX, out posY);
+
+            Position.Text = string.Format("X:-1 Y:-1", posX, posY);
+            MousePos.Text = string.Format("(-1,-1)", posMouseX, posMouseY);
+            CanPlaceLabel.Text = "範囲外です";
+        }
+
+        private void DrawCurrentColor(object sender, PaintEventArgs e)
+        {
+            SolidBrush blackBrush = new SolidBrush(Color.Black);
+            SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+            if (GameBoard.CurrentColor == Black)
+            {
+                e.Graphics.FillEllipse(blackBrush, 0, 0, 50, 50);
+            } else if (GameBoard.CurrentColor == White)
+            {
+                e.Graphics.FillEllipse(blackBrush, 0, 0, 50, 50);
+                e.Graphics.FillEllipse(whiteBrush, 2, 2, 50-4, 50-4);
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            GameBoard.ResetBoard();
+
+            panel1.Refresh();
+            CurrentColorDisplayPanel.Refresh();
+
+            LabelAmount.Text = "黒:0 白:0";
+            TurnLabel.Text = "0";
+            TipLabel.Text = "";
+        }
+    }
+
+
+    public class BoardMain
+    {
+        private int[,] Board = new int[8, 8];
+        public const int Black = 0;
+        public const int White = 1;
+        public const int None = -1;
+        public int CurrentColor = Black;
+
+        public int Turn = 0;
+
+        private int[,] DirectionVector = {
+            {0, -1}, //UP
+            {1, -1}, //UP RIGHT
+            {1, 0}, //RIGHT
+            {1, 1}, //DOWN RIGHT
+            {0, 1}, //DOWN
+            {-1, 1}, //DOWN LEFT
+            {-1, 0}, //LEFT
+            {-1, -1}, //UP LEFT
+        };
+
+
+        // Functions //
+        public void ChangeTurn()
+        {
+            CurrentColor = OppositeColor(CurrentColor);
+        }
+
+        public void ResetBoard()
+        {
+            for(int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    this.Board[x, y] = None;
+                }
+            }
+
+            this.Board[3, 3] = White;
+            this.Board[3, 4] = Black;
+            this.Board[4, 3] = Black;
+            this.Board[4, 4] = White;
+
+            this.Turn = 0;
+            this.CurrentColor = Black;
+        }
+
+        public int Color(int _x, int _y)
+        {
+            return this.Board[_x,_y];
+        }
+
+        public Boolean CanPlace(int _myColor, int _x, int _y)
+        {
+            for(int i = 0; i < 8; i++)
+            {
+                int tempX = _x;
+                int tempY = _y;
+
+                tempX += this.DirectionVector[i, 0];
+                tempY += this.DirectionVector[i, 1];
+
+                if(this.CheckBoardRange(tempX, tempY) == false)
+                {
+                    continue;
+                }
+
+                if(this.Board[_x,_y]==Black || this.Board[_x, _y] == White)
+                {
+                    return false;
+                }
+
+                if (this.Board[tempX, tempY] == this.OppositeColor(_myColor))
+                {
+                    while (true)
+                    {
+                        tempX += this.DirectionVector[i, 0];
+                        tempY += this.DirectionVector[i, 1];
+
+                        if (this.CheckBoardRange(tempX, tempY) == false)
+                        {
+                            break;
+                        }
+
+                        if (this.Board[tempX, tempY] == _myColor)
+                        {
+                            return true;
+                        }else if (this.Board[tempX, tempY] == this.OppositeColor(_myColor))
+                        {
+                        }
+                        else
+                        {
+                            break;
+                        }
+
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public void Place(int _myColor, int _x, int _y)
+        {
+            for(int i=0; i<8; i++)
+            {
+                int tempX = _x + DirectionVector[i,0];
+                int tempY = _y + DirectionVector[i, 1];
+
+                if(CheckBoardRange(tempX, tempY) == false)
+                {
+                    continue;
+                }
+
+                if (this.Board[tempX, tempY] == this.OppositeColor(_myColor))
+                {
+                    int count = 1;
+                    this.Board[_x, _y] = _myColor;
+
+                    while (true)
+                    {
+                        tempX += DirectionVector[i, 0];
+                        tempY += DirectionVector[i, 1];
+
+                        if (CheckBoardRange(tempX, tempY) == false)
+                        {
+                            break;
+                        }
+
+                        if (this.Board[tempX, tempY] == _myColor)
+                        {
+                            tempX = _x;
+                            tempY = _y;
+                            for(int j=0; j<count; j++)
+                            {
+                                tempX += DirectionVector[i, 0];
+                                tempY += DirectionVector[i, 1];
+                                this.Board[tempX, tempY] = _myColor;
+                            }
+                            break;
+                        }
+                        else if(this.Board[tempX, tempY] == None)
+                        {
+                            break;
+                        }
+
+                        count++;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+
+
+            }
+        }
+
+        public void GetGridPosition(int _MousePosX, int _MousePosY, int _DisplaySize, out int PosX, out int PosY)
+        {
+            int GridSize = _DisplaySize / 8;
+
+            if (_MousePosX == 0)
+            {
+                PosX = 0;
+            }
+            else if (_MousePosX > _DisplaySize)
+            {
+                PosX = -1;
+            }
+            else
+            {
+                PosX = (int)(_MousePosX / GridSize);
+            }
+
+            if (_MousePosY == 0)
+            {
+                PosY = 0;
+            }
+            else if (_MousePosY > _DisplaySize)
+            {
+                PosY = -1;
+            }
+            else
+            {
+                PosY = (int)(_MousePosY / GridSize);
+            }
+        }
+
+        public Boolean CheckGameEnd()
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if(this.CanPlace(Black, x, y)==true || this.CanPlace(White, x, y) == true)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public Boolean CanPlaceMyColor(int _myColor)
+        {
+            for (int x = 0; x < 8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (this.CanPlace(_myColor, x, y) == true)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public int GetAmount(int _color)
+        {
+            int count = 0;
+            for(int x=0; x<8; x++)
+            {
+                for (int y = 0; y < 8; y++)
+                {
+                    if (this.Board[x, y] == _color)
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+
+        private int OppositeColor(int _color)
+        {
+            if (_color == Black)
+            {
+                return White;
+            }
+            else if(_color == White)
+            {
+                return Black;
+            }
+            else
+            {
+                return None;
+            }
+        }
+
+        private Boolean CheckBoardRange(int _x, int _y)
+        {
+            if(_x>7 || _x<0 || _y>7 || _y < 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
+    }
+}
